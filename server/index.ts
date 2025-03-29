92265258145335a3839cde391be7fa2d08a1168d
Nicholas <nick.nogue28@outlook.com>
2025-03-29 18:28:10 -0300

    add basic structure

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: Bun.env.PORT || 3000,
+    fetch(req) {
+        console.log(req)
+        return new Response("Bun!");
+    },
+  });
+  
+  console.log(`Listening on http://localhost:${server.port} ...`);
